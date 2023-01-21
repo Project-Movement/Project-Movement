27e9172448c633c8632a07f4bdd335bb8b878df1
jgao2 <jgao222@uw.edu>
2023-01-21 15:05:23 -0800

    added bouncing off walls

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -8,17 +8,18 @@ export var ground_friction = 1100  # friction of ground
 export var max_grounded_speed = 300  # maximum speed on ground
 export var gravity = 750  # gravitational acceleration
 export var jump_vel = 400  # instantaneous velocity on jump
-export var dash_magnitude: int = 350
+export var dash_magnitude: int = 400
 
 var constant_forces = { "gravity": Vector2(0, gravity) }
 var velocity = Vector2()
 var has_double_jump: bool = true
 
+var last_tick_vel = Vector2()  # save what the engine thinks should be the new velocity after moving and sliding
 
 func _physics_process(delta):
-	apply_constant_forces(delta)
 	player_move(delta)
-	velocity = move_and_slide(velocity, Vector2.UP)
+	apply_constant_forces(delta)
+	last_tick_vel = move_and_slide(velocity, Vector2.UP)
 
 
 func _input(event):
@@ -28,6 +29,18 @@ func _input(event):
 
 func player_move(delta):
 	var grounded = is_on_floor()
+
+	# if we don't bounce, accept the engine's default move and slide velocity change
+	if not do_any_bounce():
+		velocity = last_tick_vel
+
+	# handle jump and double jump (midair jump)
+	if Input.is_action_just_pressed("jump"):
+		if grounded or has_double_jump:
+			velocity.y = -jump_vel
+
+		has_double_jump = false if not grounded else true
+
 
 	# apply grounded kinematics
 	if grounded:
@@ -50,20 +63,22 @@ func player_move(delta):
 		if Input.is_action_pressed("left") and velocity.x > -max_h_air_influence_speed:
 			velocity.x -= h_accel_air * delta
 
-
-	# handle jump and double jump (midair jump)
-	if Input.is_action_just_pressed("jump"):
-		if grounded or has_double_jump:
-			velocity.y = -jump_vel
-
-		has_double_jump = false if not grounded else true
-
 	# dash
 	if Input.is_action_just_pressed("dash"):
 		var target = get_global_mouse_position()
 		var diff = (target - self.global_position).normalized()
 
 		velocity += diff * dash_magnitude
+
+
+func do_any_bounce() -> bool:
+	var has_bounced = false
+	# handle bouncing off walls
+	if is_on_wall() and Input.is_action_pressed("jump"):
+		velocity.x = -velocity.x
+		has_bounced = true
+
+	return has_bounced
 
 
 
