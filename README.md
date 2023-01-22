a9e405e9411fb536e34fe2c5f821bcd23ca20a28
jgao2 <jgao222@uw.edu>
2023-01-21 16:51:26 -0800

    add scuffed wall sliding, wall jumping, bhopping, change wall bouncing to have min speed req

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 - WASD to move
 - Press SPACE while in air to do midair jump (double jump)
+    - Hold the opposite direction key when performing midair jump to do a Double Jump Reflect,
+      having the same effect as bouncing off of a wall
 - LSHIFT to dash, dash in direction of mouse cursor
-- Hold SPACE while hitting a wall to bounce off
+- Hold SPACE while hitting a wall or ground to bounce off, preserving momentum
--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -5,10 +5,13 @@ export var h_accel_ground = 2200  # player's horizontal acceleration
 export var h_accel_air = 200
 export var max_h_air_influence_speed = 100
 export var ground_friction = 1100  # friction of ground
+export var exceeding_ground_friction = 3000 # friction of ground when exceeding the max speed
 export var max_grounded_speed = 300  # maximum speed on ground
 export var gravity = 750  # gravitational acceleration
 export var jump_vel = 400  # instantaneous velocity on jump
 export var dash_magnitude: int = 400
+export var walljump_speed = 350
+export var wall_friction = 300
 
 var constant_forces = { "gravity": Vector2(0, gravity) }
 var velocity = Vector2()
@@ -19,6 +22,7 @@ var last_tick_vel = Vector2()  # save what the engine thinks should be the new v
 func _physics_process(delta):
 	player_move(delta)
 	apply_constant_forces(delta)
+	apply_frictions(delta)
 	last_tick_vel = move_and_slide(velocity, Vector2.UP)
 
 
@@ -36,13 +40,22 @@ func player_move(delta):
 
 	# handle jump and double jump (midair jump)
 	if Input.is_action_just_pressed("jump"):
-		if grounded or has_double_jump:
+		if grounded:
 			velocity.y = -jump_vel
+		elif is_on_wall():
+			velocity.y = -jump_vel
+			var wall_collider = get_last_slide_collision()
+			velocity.x = walljump_speed if wall_collider.normal.x > 0 else -walljump_speed
+		elif has_double_jump:  # not grounded by double jump is available
+			velocity.y = -jump_vel
+			if (Input.is_action_pressed("left") and velocity.x > 0) or (Input.is_action_pressed("right") and velocity.x < 0):
+				velocity.x = -velocity.x
 
-		has_double_jump = false if not grounded else true
+
+		has_double_jump = grounded or is_on_wall()  # if we were grounded or on wall, we didn't use double jump
 
 
-	# apply grounded kinematics
+	# grounded character movement
 	if grounded:
 		has_double_jump = true  # refresh double jump
 		if Input.is_action_pressed("right"):
@@ -50,13 +63,6 @@ func player_move(delta):
 		if Input.is_action_pressed("left"):
 			velocity.x -= h_accel_ground * delta
 
-		# apply grounded friction - force opposite direction of motion
-		if velocity.x > 0:
-			velocity.x = max(0, velocity.x + -ground_friction * delta)
-			velocity.x = min(max_grounded_speed, velocity.x)
-		if velocity.x < 0:
-			velocity.x = min(0, velocity.x + ground_friction * delta)
-			velocity.x = max(-max_grounded_speed, velocity.x)
 	else:  # not grounded, in air
 		if Input.is_action_pressed("right") and velocity.x < max_h_air_influence_speed:
 			velocity.x += h_accel_air * delta
@@ -73,14 +79,31 @@ func player_move(delta):
 
 func do_any_bounce() -> bool:
 	var has_bounced = false
-	# handle bouncing off walls
-	if is_on_wall() and Input.is_action_pressed("jump"):
+	# handle bouncing off walls, jump up, perfect reflection of x vel
+	if is_on_wall() and Input.is_action_pressed("jump") and abs(velocity.x) > max_grounded_speed * 1.2:
 		velocity.x = -velocity.x
+		# velocity.y = -jump_vel
+		has_bounced = true
+
+	# bouncing off ground, bhopping, perfect preservation of x vel
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		velocity.y = -jump_vel
 		has_bounced = true
 
 	return has_bounced
 
 
+func apply_frictions(delta):
+	if is_on_floor():
+		# apply grounded friction - force opposite direction of motion
+		var friction = ground_friction if abs(velocity.x) <= max_grounded_speed else exceeding_ground_friction
+		if velocity.x > 0:
+			velocity.x = max(0, velocity.x + -friction * delta)
+		if velocity.x < 0:
+			velocity.x = min(0, velocity.x + friction * delta)
+	elif is_on_wall() and velocity.y > 0:
+		# wall sliding, slow down player if falling down wall
+		velocity.y -= wall_friction * delta
 
 func apply_constant_forces(delta):
 	for val in constant_forces.values():
