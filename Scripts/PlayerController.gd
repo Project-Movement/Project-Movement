1f01a0a5c7c2591e94db1a574aafd206bb472457
jgao2 <jgao222@uw.edu>
2023-01-25 02:25:31 -0800

    add gliding up and down, like surfing ramps in source surf (gliding up doesn't work very well)

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -9,10 +9,15 @@ export var exceeding_ground_friction = 3000 # friction of ground when exceeding 
 export var max_grounded_speed = 300  # maximum speed on ground
 export var gravity = 750  # gravitational acceleration
 export var jump_vel = 400  # instantaneous velocity on jump
-export var dash_magnitude: int = 400
-export var walljump_speed = 350
-export var wall_friction = 300
-export var coyote_time_ms = 80
+export var dash_magnitude: int = 400  # how much the dash moves
+export var walljump_speed = 350  # x speed after walljump
+export var wall_friction = 300  # wall friction
+export var coyote_time_ms = 80  # coyote time, where player can jump despite not being grounded if they were just grounded
+export var glider_x_conversion_efficiency = 0.8  # efficiency of conversion between x and up
+export var glider_y_conversion_efficiency = 0.8  # efficiency of conversion between down and x
+export var glider_x_rate = 2  # how fast horizontal is converted to up in glider
+export var glider_y_rate = 2.7  # how fast down is converted to horizontal in glider
+export var glider_up_antigravity_factor = 0.6  # ratio of gravity that is cancelled when gliding up
 
 var constant_forces = { "gravity": Vector2(0, gravity) }
 var velocity = Vector2()
@@ -83,6 +88,24 @@ func player_move(delta):
 			velocity.x += h_accel_air * delta
 		if Input.is_action_pressed("left") and velocity.x > -max_h_air_influence_speed:
 			velocity.x -= h_accel_air * delta
+
+		# glider motion - convert horizontal to vertical or vice versa
+		if Input.is_action_pressed("up"):
+			var x_speed_lost = glider_x_rate * velocity.x * delta
+			velocity.y -= abs(x_speed_lost) * glider_x_conversion_efficiency
+			velocity.x -= x_speed_lost
+			velocity.y -= gravity * delta * glider_up_antigravity_factor  # counter some of gravity
+
+		elif Input.is_action_pressed("down"):  # can't do both up and down
+			# this one only works when falling
+			if velocity.y > 0:
+				var y_speed_lost = glider_y_rate * velocity.y * delta
+				velocity.y -= y_speed_lost
+				if velocity.x > 0:
+					velocity.x += y_speed_lost * glider_y_conversion_efficiency
+				elif velocity.x < 0:
+					velocity.x -= y_speed_lost * glider_y_conversion_efficiency
+				# TODO: how to handle when player is falling straight down?
 
 	# dash
 	if Input.is_action_just_pressed("dash"):
