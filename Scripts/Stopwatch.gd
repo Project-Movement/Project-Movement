537a4c2b01761888969d1ba9b07d8ab1a3b1fa63
jgao2 <jgao222@uw.edu>
2023-01-30 22:06:17 -0800

    add start and end zones changing stopwatch time

--- Scripts/Stopwatch.gd
+++ Scripts/Stopwatch.gd
@@ -1,12 +1,39 @@
 extends CanvasLayer
 
 var time = 0  # time in seconds
+var active = false
 
 func _process(delta):
-	time += delta
+	if active:
+		time += delta
 
 	var minutes = round(time / 60)
 
 	$Time.text = "%02d:%05.2f" % [minutes, time]
 
 
+func reset():
+	print("reset stopwatch")
+	time = 0
+
+
+func stop():
+	active = false
+
+
+func start():
+	print("started stopwatch")
+	active = true
+
+
+
+func _on_StartZone_body_entered(_body:Node):
+	reset()
+	stop()
+
+
+func _on_StartZone_body_exited(_body:Node):
+	start()
+
+func _on_EndZone_body_entered(_body:Node):
+	stop()
