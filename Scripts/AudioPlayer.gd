6f664f83befbde380b1a199b404cedc6ad5791b4
jgao2 <jgao222@uw.edu>
2023-02-09 17:58:51 -0800

    change jump sound, add sound for touching killzone

--- Scripts/AudioPlayer.gd
+++ Scripts/AudioPlayer.gd
@@ -3,11 +3,13 @@ extends Node
 
 # onready var STEP: = preload("res://Sounds/footstep_carpet_001.ogg")
 var STEP
-onready var JUMP: = preload("res://Sounds/impactPlank_medium_000.ogg")
+# onready var JUMP: = preload("res://Sounds/impactPlank_medium_000.ogg")
+onready var JUMP: = preload("res://Sounds/jump.wav")
 onready var CHECKPOINT: = preload("res://Sounds/impactMining_003.ogg")
 onready var DASH: = preload("res://Sounds/laserSmall_000.ogg")
 onready var STOPWATCH_STOP: = preload("res://Sounds/toggle_002.ogg")
 onready var STOPWATCH_START: = preload("res://Sounds/toggle_001.ogg")
+onready var DIE: = preload("res://Sounds/die.wav")
 
 
 onready var stream_players: = $StreamPlayers
--- Scripts/KillZone.gd
+++ Scripts/KillZone.gd
@@ -3,4 +3,6 @@ extends Area2D
 
 func _on_KillZone_body_entered(_body:Node):
 	if _body.name == "PlayerBody":
+		AudioPlayer.play_sound(AudioPlayer.DIE)
+		# yield(get_tree().create_timer(0.3), "timeout")
 		PlayerSpawner.respawn_player()
