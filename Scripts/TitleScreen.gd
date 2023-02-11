1055ec20067458dbebff9b1be7f73db3d8bc148d
jgao2 <jgao222@uw.edu>
2023-02-10 22:26:19 -0800

    change title button names to reflect new tutorials

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -6,11 +6,11 @@ func go_tutorial():
 	var _a = get_tree().change_scene("res://Scenes/Tutorial.tscn")
 
 
-func go_level1():
+func go_adv_tutorial():
 	var _a = get_tree().change_scene("res://Scenes/AdvancedTutorial.tscn")
 
 
-func go_testlevel():
+func go_level1():
 	var _a = get_tree().change_scene("res://Scenes/Level1.tscn")
 
 
@@ -19,5 +19,4 @@ func quit():
 
 
 func _on_DevSettings_pressed():
-
 	var _a = get_tree().change_scene("res://Scenes/DevSettings.tscn")
