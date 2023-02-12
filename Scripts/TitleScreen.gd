2d93b6d88418a56cedfccf16b2281a3343cbaa2b
Chaos Gao <gyq2018@uw.edu>
2023-02-12 01:26:54 -0800

    Add in botton

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -5,14 +5,17 @@ extends Node2D
 func go_tutorial():
 	var _a = get_tree().change_scene("res://Scenes/Tutorial.tscn")
 
-
-func go_level1():
+func go_tutorial2():
 	var _a = get_tree().change_scene("res://Scenes/AdvancedTutorial2.tscn")
 
+func go_dearYouWallJump():
+	var _a = get_tree().change_scene("res://Scenes/DearYouWallJump.tscn")
 
-func go_testlevel():
+func go_circus():
 	var _a = get_tree().change_scene("res://Scenes/Circus.tscn")
 
+func go_level3():
+	var _a = get_tree().change_scene("res://Scenes/Level1.tscn")
 
 func quit():
 	get_tree().quit()
@@ -21,3 +24,7 @@ func quit():
 func _on_DevSettings_pressed():
 
 	var _a = get_tree().change_scene("res://Scenes/DevSettings.tscn")
+
+
+func _on_TutorialAdvance_pressed():
+	pass # Replace with function body.
