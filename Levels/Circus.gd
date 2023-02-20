c59a47cb00fc9dc994b05a137211155680c58fc6
jgao2 <jgao222@uw.edu>
2023-02-20 15:33:27 -0800

    delete old unused scripts that somehow got back in

--- Levels/Circus.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-
-func _ready():
-	Logger.log_level_start(Logger.LEVELS.CIRCUS, str(Logger.cur_time_millis()))
--- Levels/TestLevel.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	# Logger.start_new_session()
-	# Logger.log_action_with_no_level(41, JSON.print({"gabba": "gook", "tenthy": 3}))
-	Logger.log_level_start(Logger.LEVELS.TESTLEVEL, str(Logger.cur_time_millis()))
